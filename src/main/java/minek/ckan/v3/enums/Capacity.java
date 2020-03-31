112042531c6f12836e819f009f56abf7197990a1
이용홍 <hong@mine-k.co.kr>
2020-03-31 16:51:43 +0900

    docs : Capacity 설명 url 추가
    get api 호출해서 받은 Capacity 는 Package, User, Group 이 혼재 되어 있다.
    무리해서 enum 을 분리할 필요는 없지만 개념은 알아두도록 하자.

--- src/main/java/minek/ckan/v3/enums/Capacity.java
+++ src/main/java/minek/ckan/v3/enums/Capacity.java
@@ -2,6 +2,7 @@ package minek.ckan.v3.enums;
 
 import lombok.Getter;
 
+// https://github.com/ckan/ckan/blob/ef103f02292e0b50fbe7edc6a9e07f70d2fb9f45/ckan/model/group.py#L61
 public enum Capacity implements EnumCode {
     member("member"),
     editor("editor"),
